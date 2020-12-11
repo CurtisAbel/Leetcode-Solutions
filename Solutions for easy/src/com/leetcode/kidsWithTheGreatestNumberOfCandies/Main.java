5e5da29c61d333b8f0a2eea5fcabd5b92ba2eaee
Curtis Lavaud <curtislavaud@outlook.com>
2020-12-11 19:49:16 +0000

    kids with the greatest number of candies challenge
    Solution for kids with the greatest number of candies challenge

---
+++ Solutions for easy/src/com/leetcode/kidsWithTheGreatestNumberOfCandies/Main.java
@@ -0,0 +1,49 @@
+package com.leetcode.kidsWithTheGreatestNumberOfCandies;
+
+import java.util.*;
+
+public class Main {
+public static void main(String[] args) {
+	
+	int [] myArr = {2,3,5,1,3};
+	
+	kidsWithCandies(myArr, 3);
+	
+	
+	
+}
+public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    
+	List<Boolean> isHigherThanGreatestCandy = new ArrayList<>();
+	int greatest =0;
+	
+	//loop to find child with most candies within array
+	for(int i = 0; i<candies.length;i++) {
+		if(candies[i]>greatest) {
+			
+			greatest=candies[i];
+			
+		}
+			}
+	
+	//checks if extra candies are higher than child with greatest candies
+	for(int j =0; j<candies.length;j++ ) {
+		if(candies[j] + extraCandies>=greatest) {
+			isHigherThanGreatestCandy.add(true);
+			
+			
+		}else {
+			
+			isHigherThanGreatestCandy.add(false);
+		}
+		
+		
+	}
+
+	System.out.println(isHigherThanGreatestCandy);
+	return isHigherThanGreatestCandy;
+	
+}
+
+
+}
--- Solutions for easy/src/com/leetcode/runningsum/Main.java
+++ Solutions for easy/src/com/leetcode/runningsum/Main.java
@@ -4,22 +4,20 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		int [] myArr = {1,2,3,4};
+		int[] myArr = { 1, 2, 3, 4 };
 		runningSum(myArr);
-		
+
 	}
-	 public static int[] runningSum(int[] nums) {
-		 
-	     for(int i =1; i<nums.length;i++) {
-	    	 
-	    	 nums[i]+=nums[i-1];
-	    	 
-	    	 System.out.println(nums[i]);
-	    	 
-	     }
-	     System.out.println(Arrays.toString(nums));
-		 return nums;
-	    }
-	
+
+	public static int[] runningSum(int[] nums) {
+
+		for (int i = 1; i < nums.length; i++) {
+
+			nums[i] += nums[i - 1];
+
+		}
+	//	System.out.println(Arrays.toString(nums));
+		return nums;
+	}
 
 }
