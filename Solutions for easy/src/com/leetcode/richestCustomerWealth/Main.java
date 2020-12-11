95af64607554ffb7b91e6a4e9f9c5cd6d8541e48
Curtis Lavaud <curtislavaud@outlook.com>
2020-12-11 19:27:47 +0000

    running sum challenge
    My solution for running sum challenge

--- Solutions for easy/src/com/leetcode/richestCustomerWealth/Main.java
+++ Solutions for easy/src/com/leetcode/richestCustomerWealth/Main.java
@@ -2,32 +2,32 @@ package com.leetcode.richestCustomerWealth;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		int [][] accounts = {{2,8,7},{7,1,3},{1,9,5}};
-		
+
+		int[][] accounts = { { 2, 8, 7 }, { 7, 1, 3 }, { 1, 9, 5 } };
+
 		System.out.println(maximumWealth(accounts));
-		
+
 	}
-	
-    public static int maximumWealth(int[][] accounts) {
-    	int wealthiest =0;
-    	int sum;
-    	for(int row = 0; row<accounts.length; row++) {
-    		sum=0;
-    		for(int col =0; col<accounts[row].length; col++) {
-    				sum+= accounts[row][col];
-    		}
-    		if(sum > wealthiest) {
-				
-				wealthiest=sum;
-				
-				
-				
+
+	public static int maximumWealth(int[][] accounts) {
+		int wealthiest = 0;
+		int sum;
+		for (int row = 0; row < accounts.length; row++) {
+			sum = 0; // resets sum back to 0 after loop
+			for (int col = 0; col < accounts[row].length; col++) {
+				sum += accounts[row][col];
 			}
-			
-    	}
-    	
-        return wealthiest;
-    }
+
+			// compare each sum to the highest known wealthiest person
+			if (sum > wealthiest) {
+
+				wealthiest = sum;
+
+			}
+
+		}
+
+		return wealthiest;
+	}
 
 }
---
+++ Solutions for easy/src/com/leetcode/runningsum/Main.java
@@ -0,0 +1,25 @@
+package com.leetcode.runningsum;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int [] myArr = {1,2,3,4};
+		runningSum(myArr);
+		
+	}
+	 public static int[] runningSum(int[] nums) {
+		 
+	     for(int i =1; i<nums.length;i++) {
+	    	 
+	    	 nums[i]+=nums[i-1];
+	    	 
+	    	 System.out.println(nums[i]);
+	    	 
+	     }
+	     System.out.println(Arrays.toString(nums));
+		 return nums;
+	    }
+	
+
+}
